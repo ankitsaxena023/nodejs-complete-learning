1556e5c892314619a1899a5b1318d6126956b721
Ankit Saxena <ankit.saxena@congnitiveclouds.com>
2023-12-12 19:30:36 +0000

    Dec 13, 2023, 1:00 AM

--- Day-1_Basics-of-nodejs/05-parsing-data-in-file/app.js
+++ Day-1_Basics-of-nodejs/05-parsing-data-in-file/app.js
@@ -26,7 +26,7 @@ const server = http.createServer((req, res) => {
       const parsedBody = Buffer.concat(body).toString();
       const message = parsedBody.split("=")[1];
 
-      // Note: 1. line number 34 executes after line number 37-39 code, means it'll executes after response sent because it's an synchronous method of node js and due to which it'll not wait in event loop it'll executes immediately later we solve this problem with writeFile method
+      // Note: 1. line number 31 executes after line number 34-36 code, means it'll executes after response sent because it's an synchronous method of node js and due to which it'll not wait in event loop it'll executes immediately later we solve this problem with writeFile method
 
       fs.writeFileSync("message.txt", message);
     });
