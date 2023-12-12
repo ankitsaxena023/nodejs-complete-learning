5e8cfeece65c1f785eef70a101bda2b21d22932e
Ankit Saxena <ankit.saxena@congnitiveclouds.com>
2023-12-12 21:00:32 +0000

    Dec 13, 2023, 2:30 AM

--- Day-3_Working-with-express.js/01-install-express.js/app.js
+++ Day-3_Working-with-express.js/01-install-express.js/app.js
@@ -2,6 +2,11 @@ const http = require("http");
 const express = require("express");
 
 // make an express application here
+
+//Note : express js is all about middleware where middleware means incoming requests funneled(move, pass) through a bunch of functions so basically you can have multiple functions of request handlers until you send a response
+
+// This simply means express js provide you functionality to plugged in with other third party libraries which can connect with functions to your application and provide many functionalities.
+
 const app = express();
 
 // so for plugged in other libraries to application with we use "app.use()" but after creating app object but before pass it to create server, use allow us to add new middleware function
@@ -20,7 +25,3 @@ app.use((req, res, next) => {
 const server = http.createServer(app);
 
 server.listen(3000);
-
-//Note : express js is all about middleware where middleware means incoming requests funneled(move, pass) through a bunch of functions so basically you can have multiple functions of request handlers until you send a response
-
-// This simply means express js provide you functionality to plugged in with other third party libraries which can connect with functions to your application and provide many functionalities.
