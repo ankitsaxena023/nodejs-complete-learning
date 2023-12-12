8f6c8334e20aa734191ef227d17fdde6aed00bba
Ankit Saxena <ankit.saxena@congnitiveclouds.com>
2023-12-12 20:47:02 +0000

    Dec 13, 2023, 2:17 AM

--- Day-3_Working-with-express.js/01-install-express.js/app.js
+++ Day-3_Working-with-express.js/01-install-express.js/app.js
@@ -10,4 +10,4 @@ server.listen(3000);
 
 //Note : express js is all about middleware where middleware means incoming requests funneled(move, pass) through a bunch of functions so basically you can have multiple functions of request handlers until you send a response
 
-//
+// This simply means express js provide you functionality to plugged in with other third party libraries which can connect with functions to your application and provide many functionalities.
