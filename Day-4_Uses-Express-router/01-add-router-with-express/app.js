cd2ba3815f32cec8c303476079c88d0eba9b1251
Ankit Saxena <ankit.saxena@congnitiveclouds.com>
2023-12-13 16:07:04 +0000

    Dec 13, 2023, 9:37 PM

--- Day-4_Uses-Express-router/01-add-router-with-express/app.js
+++ Day-4_Uses-Express-router/01-add-router-with-express/app.js
@@ -13,5 +13,4 @@ app.use(shopRoutes);
 
 app.listen(3000);
 
-// express.Router() method provide router which we use for handle our get post and other works
-
+// express.Router() method provide router which we use for handle our get post and other works in other words with the help of express.Router() we manage our routes
