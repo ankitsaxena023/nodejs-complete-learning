19f2f3bbd09e8a4774563443258df2b95c441fa0
Ankit Saxena <ankit.saxena@congnitiveclouds.com>
2023-12-13 15:56:18 +0000

    Dec 13, 2023, 9:26 PM

--- Day-4_Uses-Express-router/01-add-router-with-express/app.js
+++ Day-4_Uses-Express-router/01-add-router-with-express/app.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const adminRoutes = require("./routes/admin");
+
 const bodyParser = require("body-parser");
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use();
 
 app.use("/", (req, res, next) => {
   res.send("<h1>Hello from Express.js</h1>");
