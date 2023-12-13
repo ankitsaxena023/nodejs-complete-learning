d67fb7d1dbae862a91df0d9349cd30fb154e648b
Ankit Saxena <ankit.saxena@congnitiveclouds.com>
2023-12-13 11:25:25 +0000

    Dec 13, 2023, 4:55 PM

--- app.js
+++ app.js
@@ -5,13 +5,7 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false })); // this body-parser parse the data like form data for us which we've been parsing our own in the previous section but for other kind of data parsing like file data we'll use different parser, and this makes express so extensible
 
-app.use("/", (req, res, next) => {
-  console.log("1. This always runs!");
-  next();
-});
-
 app.use("/add-product", (req, res, next) => {
-  console.log("2. In another middleware");
   // res.send('<h1>The "Add Product" page</h1>');
   // let's make above code a form
   res.send(
