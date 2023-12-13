a74c1418ecb5d2f863f98bf33abda40076d77b6a
Ankit Saxena <ankit.saxena@congnitiveclouds.com>
2023-12-13 10:45:19 +0000

    Dec 13, 2023, 4:15 PM

---
+++ Day-3_Working-with-express.js/03-handling-parsing-request/app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+
+const app = express();
+
+app.use("/", (req, res, next) => {
+  console.log("1. This always runs!");
+  next();
+});
+
+app.use("/add-product", (req, res, next) => {
+  console.log("2. In another middleware");
+  // res.send('<h1>The "Add Product" page</h1>');
+  // let's make above code a form
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
+  );
+});
+
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
+  console.log("3. again another middleware");
+  res.send("<h1>Hello from Express.js</h1>");
+});
+
+app.listen(3000);
