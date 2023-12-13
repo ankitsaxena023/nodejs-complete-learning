1e5d6597c006039b908add01a1417f207dc456c2
Ankit Saxena <ankit.saxena@congnitiveclouds.com>
2023-12-13 10:42:11 +0000

    Dec 13, 2023, 4:12 PM

--- app.js
+++ app.js
@@ -16,6 +16,11 @@ app.use("/add-product", (req, res, next) => {
   );
 });
 
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
 app.use("/", (req, res, next) => {
   console.log("3. again another middleware");
   res.send("<h1>Hello from Express.js</h1>");
