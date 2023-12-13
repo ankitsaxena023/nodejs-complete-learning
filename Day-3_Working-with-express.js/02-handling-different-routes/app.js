587a444ee6f2858ede378cd2853e5b7dd2aba0eb
Ankit Saxena <ankit.saxena@congnitiveclouds.com>
2023-12-13 08:02:39 +0000

    Dec 13, 2023, 1:32 PM

--- Day-3_Working-with-express.js/02-handling-different-routes/app.js
+++ Day-3_Working-with-express.js/02-handling-different-routes/app.js
@@ -3,13 +3,15 @@ const express = require("express");
 const app = express();
 
 app.use((req, res, next) => {
-  console.log("In the middleware");
+  // console.log("In the middleware");
   next();
 });
 
 app.use((req, res, next) => {
-  console.log("In another middleware");
+  // console.log("In another middleware");
   res.send("<h1>Hello from Express.js</h1>");
 });
+
+console.log("outside the middleware")
 
 app.listen(3000);
