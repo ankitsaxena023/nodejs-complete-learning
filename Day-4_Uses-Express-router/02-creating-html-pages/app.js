5d375a1ff4436a52875800dcccdf57d94775d302
Ankit Saxena <ankit.saxena@congnitiveclouds.com>
2023-12-14 06:55:51 +0000

    Dec 14, 2023, 12:25 PM

---
+++ Day-4_Uses-Express-router/02-creating-html-pages/app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Page not found!</h1>"); //so here we can send code for handle error and we can use chain of methods here like we did status but always remember send should be the last one
+});
+
+app.listen(3000);
+
+//express.Router() method provide router which we use for handle our get post and other works in other words with the help of express.Router() we manage our routes
---
+++ Day-4_Uses-Express-router/02-creating-html-pages/routes/admin.js
@@ -0,0 +1,40 @@
+const express = require("express");
+
+const router = express.Router();
+
+// 1. Normal path
+
+// router.get("/add-product", (req, res, next) => {
+//   res.send(
+//     '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
+//   );
+// });
+
+// router.post("/product", (req, res, next) => {
+//   console.log("reqBody", req.body);
+//   res.redirect("/");
+// });
+
+// 2. Admin path
+
+// Note : Some points to remember
+// 1) we can use same path for different routes (like here for get and post)
+// 2) we also need to change our form action "admin/add-product" because we're reaching to post request with the admin
+// 3) here we can also extract our admin/ path and put it to app.js adminRoutes file
+
+// router.get("/admin/add-product", (req, res, next) => {
+
+// admin/add-product => GET
+router.get("/add-product", (req, res, next) => {
+  res.send(
+    '<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
+  );
+});
+
+// admin/add-product => POST
+router.post("/add-product", (req, res, next) => {
+  console.log("reqBody", req.body);
+  res.redirect("/");
+});
+
+module.exports = router;
