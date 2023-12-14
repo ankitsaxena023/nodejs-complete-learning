33748b2c6abd39f89e664cb20b00c03d8eeb08c1
Ankit Saxena <ankit.saxena@congnitiveclouds.com>
2023-12-14 06:20:47 +0000

    Dec 14, 2023, 11:50 AM

--- Day-4_Uses-Express-router/01-add-router-with-express/routes/admin.js
+++ Day-4_Uses-Express-router/01-add-router-with-express/routes/admin.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const router = express.Router();
 
-//Normal
+// 1. Normal path
 router.get("/add-product", (req, res, next) => {
   res.send(
     '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
@@ -10,6 +10,19 @@ router.get("/add-product", (req, res, next) => {
 });
 
 router.post("/product", (req, res, next) => {
+  console.log("reqBody", req.body);
+  res.redirect("/");
+});
+
+// 2. Admin path
+
+router.get("/admin/add-product", (req, res, next) => {
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
+  );
+});
+
+router.post("/add-product", (req, res, next) => {
   console.log("reqBody", req.body);
   res.redirect("/");
 });
