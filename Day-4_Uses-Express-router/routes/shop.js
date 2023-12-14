9071840fb4b5fa6369f28bfdead07e14b5b6f202
Ankit Saxena <ankit.saxena@congnitiveclouds.com>
2023-12-14 08:06:36 +0000

    Dec 14, 2023, 1:36 PM

--- Day-4_Uses-Express-router/routes/shop.js
+++ Day-4_Uses-Express-router/routes/shop.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.get("/", (req, res, next) => {
   // res.send("<h1>Hello from Express.js</h1>"); // Now we can send our file instead of raw data
-  res.sendFile(path.join(__direc));
+  res.sendFile(path.join(__dirname, "../", "views", "shop.html"));
 });
 
 module.exports = router;
