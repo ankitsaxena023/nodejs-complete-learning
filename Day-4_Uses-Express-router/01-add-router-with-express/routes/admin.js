e121e98ac4b481eb911c7899113a08be3908d3e7
Ankit Saxena <ankit.saxena@congnitiveclouds.com>
2023-12-14 06:22:41 +0000

    Dec 14, 2023, 11:52 AM

--- Day-4_Uses-Express-router/01-add-router-with-express/routes/admin.js
+++ Day-4_Uses-Express-router/01-add-router-with-express/routes/admin.js
@@ -16,13 +16,17 @@ router.post("/product", (req, res, next) => {
 
 // 2. Admin path
 
+// Note : Some points to remember
+// 1) we can use same path for different routes (like here for get and post)
+// 2) we also need to change our form action "add-product"
+
 router.get("/admin/add-product", (req, res, next) => {
   res.send(
     '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
   );
 });
 
-router.post("/add-product", (req, res, next) => {
+router.post("/admin/add-product", (req, res, next) => {
   console.log("reqBody", req.body);
   res.redirect("/");
 });
