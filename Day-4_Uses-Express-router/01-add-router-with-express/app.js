32c94028f9039f6f3f11d8f23b842c2c5ee2be94
Ankit Saxena <ankit.saxena@congnitiveclouds.com>
2023-12-14 04:30:28 +0000

    Dec 14, 2023, 10:00 AM

--- Day-4_Uses-Express-router/01-add-router-with-express/app.js
+++ Day-4_Uses-Express-router/01-add-router-with-express/app.js
@@ -12,9 +12,9 @@ app.use(adminRoutes);
 app.use(shopRoutes);
 
 app.use("/", (req, res, next) => {
-  res.status(404).send("<h1>Page not found!</h1>"); // so here we can send code for handle error and we can use chain of methods here like we did status but always remember send should be the last one
+  res.status(404).send("<h1>Page not found!</h1>"); //so here we can send code for handle error and we can use chain of methods here like we did status but always remember send should be the last one
 });
 
 app.listen(3000);
 
-// express.Router() method provide router which we use for handle our get post and other works in other words with the help of express.Router() we manage our routes
+//express.Router() method provide router which we use for handle our get post and other works in other words with the help of express.Router() we manage our routes
