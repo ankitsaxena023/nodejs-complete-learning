9b38bde74406aa9ff7926dc847bb63d73d90513f
Ankit Saxena <ankit.saxena@congnitiveclouds.com>
2023-12-14 06:28:25 +0000

    Dec 14, 2023, 11:58 AM

--- Day-4_Uses-Express-router/01-add-router-with-express/routes/admin.js
+++ Day-4_Uses-Express-router/01-add-router-with-express/routes/admin.js
@@ -22,13 +22,13 @@ router.post("/product", (req, res, next) => {
 // 3) here we can also extract our admin/ path and put it to app.js adminRoutes file
 
 // router.get("/admin/add-product", (req, res, next) => {
-router.get("/admin/add-product", (req, res, next) => {
+router.get("/add-product", (req, res, next) => {
   res.send(
     '<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
   );
 });
 
-router.post("/admin/add-product", (req, res, next) => {
+router.post("/add-product", (req, res, next) => {
   console.log("reqBody", req.body);
   res.redirect("/");
 });
