82b807f182ffcca9c57614018eeffb6ad1c31e61
Ankit Saxena <ankit.saxena@congnitiveclouds.com>
2023-12-14 08:30:57 +0000

    Dec 14, 2023, 2:00 PM

--- Day-4_Uses-Express-router/routes/admin.js
+++ Day-4_Uses-Express-router/routes/admin.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const rootDir = require("../util/path");
 
 const router = express.Router();
 
@@ -30,7 +31,7 @@ router.get("/add-product", (req, res, next) => {
   // res.send(
   //   '<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
   // );
-  res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
+  res.sendFile(path.join(rootDir, "views", "add-product.html"));
 });
 
 // admin/add-product => POST
