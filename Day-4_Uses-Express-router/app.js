0b9313651ea07bbca11ee29e9ea5c20728899b78
Ankit Saxena <ankit.saxena@congnitiveclouds.com>
2023-12-14 08:38:38 +0000

    Dec 14, 2023, 2:08 PM

--- Day-4_Uses-Express-router/app.js
+++ Day-4_Uses-Express-router/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const rootDir = require("./util/path");
 const bodyParser = require("body-parser");
 
 const app = express();
@@ -13,7 +14,7 @@ app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "error.html")); //so here we can send code for handle error and we can use chain of methods here like we did status but always remember send should be the last one
+  res.status(404).sendFile(path.join(rootDir, "views", "error.html")); //so here we can send code for handle error and we can use chain of methods here like we did status but always remember send should be the last one
 });
 
 app.listen(3000);
