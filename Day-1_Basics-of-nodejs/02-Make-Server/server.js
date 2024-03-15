24e0c262b0d632fe15c093534a1b4f9b1adebdb0
Ankit Saxena <ankit.saxena@congnitiveclouds.com>
2024-03-15 09:37:22 +0000

    Mar 15, 2024, 3:07 PM

--- Day-1_Basics-of-nodejs/02-Make-Server/server.js
+++ Day-1_Basics-of-nodejs/02-Make-Server/server.js
@@ -9,5 +9,12 @@ const server = http.createServer((req, res) => {
   // res.write("<body><h1>Hello from my Node.js Server</h1></body>");
   // res.write("</html>");
   // res.end();
+
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>First Node Page</title></head>");
+  res.write("<body><h1>Hello from Node.js Server</h1></body>");
+  res.write("</html>");
+  res.end();
 });
 server.listen(3000);
