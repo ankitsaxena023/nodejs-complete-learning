27c8403a2fb3d6bb4e5e1ad9ff672c56ab62930d
Ankit Saxena <ankit.saxena@congnitiveclouds.com>
2024-04-04 12:34:02 +0000

    Apr 4, 2024, 6:04 PM

--- Day-3_Working-with-express.js/app.js
+++ Day-3_Working-with-express.js/app.js
@@ -1,19 +1,33 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-// app.use("/", (req, res, next) => {
-//   console.log("1. This always runs!");
-//   next();
-// });
+app.use(bodyParser.urlencoded({ extended: false })); // this body-parser parse the data like form data for us which we've been parsing our own in the previous section but for other kind of data parsing like file data we'll use different parser, and this makes express so extensible
 
-app.use("/add-product", (req, res, next) => {
-  console.log("2. In another middleware");
-  res.send('<h1>The "Add Product" page</h1>');
+app.get("/add-product", (req, res, next) => {
+  // console.log("2. In another middleware");
+  // res.send('<h1>The "Add Product" page</h1>');
+  // let's make above code a form
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
+  );
+});
+
+// app.use("/product", (req, res, next) => {
+
+//Note: here app also give us app.get and app.post filter so that we can filter our request according to our need, so below function only will fire for POST request because /product is an POST method as mentioned /add-product and like get and post http methods app has put, patch, delete.
+
+// Remember: If we just use app.use() not app.get(), app.post() or any other then app.use handles all the http methods mean it'll handle then get, post, put, patch, delete etc.
+
+app.post("/product", (req, res, next) => {
+  console.log(req.body);
+  // console.log(req.body.title);
+  res.redirect("/");
 });
 
 app.use("/", (req, res, next) => {
-  console.log("3. again another middleware");
+  // console.log("3. again another middleware");
   res.send("<h1>Hello from Express.js</h1>");
 });
 
